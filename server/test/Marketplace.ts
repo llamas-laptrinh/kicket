6d2cf343e0d1db36820382d405c5795918c4f447
lammas-laptrinh <danghoangson51@gmail.com>
2023-12-30 16:20:04 +0700

    update test

--- server/test/Marketplace.ts
+++ server/test/Marketplace.ts
@@ -5,14 +5,28 @@ describe("Marketplace", () => {
   let marketplace: NFTMarketplace;
 
   it("get Contact", async () => {
-    marketplace = await ethers.getContractAt("NFTMarketplace", marketAddress);
+    const [signer] = await ethers.getSigners();
+    marketplace = await ethers.getContractAt(
+      "NFTMarketplace",
+      marketAddress,
+      signer
+    );
   });
   it("get all NFTs", async () => {
     const tx = await marketplace.getAllNFTs();
     console.log(tx);
   });
   it("mint NFT", async () => {
-    marketplace.createToken("", 200);
+    const price = ethers.parseUnits("0.0001", "ether");
+    const tx = await marketplace.createToken(
+      "https://lavender-cautious-ant-106.mypinata.cloud/ipfs/QmdKpwfWYmkZVBEiCDSSgXZwRAXWvaWnzawZzxAfWcEqid?pinataGatewayToken=hmcS97lnQbaQI8TIiAmLLpF7cRxrPDWW2N1KsMTqsE5SsKdG74eyTqqBKk4wTfME",
+      price,
+      { gasLimit: 40000000, value: ethers.parseUnits("0.01", "ether") }
+    );
+    await tx.wait();
   });
-  it("get my NFTs", async () => {});
+  it("get my NFTs", async () => {
+    const tx = await marketplace.getMyNFTs();
+    console.log(tx);
+  });
 });
